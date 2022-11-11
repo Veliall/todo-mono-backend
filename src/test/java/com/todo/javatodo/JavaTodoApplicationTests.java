6c0cbec4019eda652c63d1e9e5d611604bf7bf77
Христюк Игорь Анатольевич <ikhristiuk@gmail.com>
2022-11-11 14:31:48 +0300

    initial project

---
+++ src/test/java/com/todo/javatodo/JavaTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.javatodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
