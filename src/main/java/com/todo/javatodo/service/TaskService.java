af14bead49361d5cb9314f29e651229ba952ebaf
Христюк Игорь Анатольевич <khristyuk@gnivc.ru>
2022-11-13 21:15:36 +0300

    tasks

--- src/main/java/com/todo/javatodo/service/TaskService.java
+++ src/main/java/com/todo/javatodo/service/TaskService.java
@@ -5,11 +5,8 @@ import com.todo.javatodo.exception.TodoException;
 import com.todo.javatodo.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
 import java.util.List;
 
 @Service
@@ -49,11 +46,6 @@ public class TaskService {
 
     public void deleteById(Long id) {
         repository.deleteById(id);
-    }
-
-    @Cacheable(cacheNames = "tasks")
-    public Page<Task> findByParams(String text, Boolean completed, Long priorityId, Long categoryId, String email, Date dateFrom, Date dateTo, PageRequest paging) {
-        return repository.findByParams(text, completed, priorityId, categoryId, email, dateFrom, dateTo, paging);
     }
 
     public Task findById(Long id) {
